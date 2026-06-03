c09dca7ca08e6c3efdb92ac12b23581c55c51845
M4GeeZ <mughees1275@gmail.com>
2026-06-04 00:20:11 +0500

    frontend complete

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -2,7 +2,7 @@ import React, { createContext, useContext, useState } from "react";
 import axios from "axios";
 
 const AuthContext = createContext();
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+const API_URL = import.meta.env.VITE_API_URL || 'https://studentlms-x2za.onrender.com';
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(() => {
