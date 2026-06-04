2a579e764cf80b793cd858d827c50b187a2016ed
M4GeeZ <mughees1275@gmail.com>
2026-06-04 11:27:09 +0500

    updated code

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -2,7 +2,6 @@ import React, { createContext, useContext, useState } from "react";
 import axios from "axios";
 
 const AuthContext = createContext();
-const API_URL = import.meta.env.VITE_API_URL;
 const API_URL = import.meta.env.VITE_API_URL || 'https://studentlms-x2za.onrender.com';
 
 export const AuthProvider = ({ children }) => {
